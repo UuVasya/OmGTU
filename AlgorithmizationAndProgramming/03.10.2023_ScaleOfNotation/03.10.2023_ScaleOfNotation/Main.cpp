934aa4a74575158abb30392800f396b03d3245d9
UuVasya <kazru32@gmail.com>
2023-10-03 17:48:09 +0600

    correction

--- AlgorithmizationAndProgramming/03.10.2023_ScaleOfNotation/03.10.2023_ScaleOfNotation/Main.cpp
+++ AlgorithmizationAndProgramming/03.10.2023_ScaleOfNotation/03.10.2023_ScaleOfNotation/Main.cpp
@@ -43,7 +43,7 @@ void main()
 
 	for (int i = 0; i < SizeMass; i++)// Первое Задание
 	{
-		if((Mass[i] % 3) == (Mass[i] % 5)|| (Mass[i] % 5) == (Mass[i] % 7))
+		if(((Mass[i] % 3) == (Mass[i] % 5)) && ((Mass[i] % 5) == (Mass[i] % 7)))
 		{
 			ScaleOfNorm++;
 		}
